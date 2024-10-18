05a0c77afadeedc7d3b14039e899a9fafe2d18ad
Kmadhu129 <163731657+Kmadhu129@users.noreply.github.com>
2024-10-18 13:12:16 +0530

    Add files via upload
    2300090088_K. Madhumitha

---
+++ calc.js
@@ -0,0 +1,19 @@
+let a=10;
+let b=5;
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(8,9));
+function sub(a,b){
+    return a-b;
+}
+console.log(sub(8,9));
+function mul(a,b){
+    return a*b;
+}
+console.log(mul(8,9));
+function div(a,b){
+    return a/b;
+}
+console.log(div(8,9));
